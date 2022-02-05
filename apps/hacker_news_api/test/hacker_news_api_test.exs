c0edf483378265177394ea6011a6640486759fc9
noe perez chamorro <noeperezchamorro@noes-Mac-mini.local>
2022-02-05 14:26:27 -0600

    set_up test for api

--- apps/hacker_news_api/test/hacker_news_api_test.exs
+++ apps/hacker_news_api/test/hacker_news_api_test.exs
@@ -1,8 +1,23 @@
 defmodule HackerNewsApiTest do
   use ExUnit.Case
-  doctest HackerNewsApi
 
-  test "greets the world" do
-    assert HackerNewsApi.hello() == :world
+  test "fetch_top_50_news" do
+    news_top_50 = HackerNewsApi.get_top_50()
+    assert Enum.count(news_top_50) == 50
+  end
+
+  test "sorter_order_top_50_news" do
+    news_top_50 = HackerNewsApi.get_top_50()
+    assert Enum.sort(news_top_50, :desc) == news_top_50
+  end
+
+  test "fecth_news_by_id" do
+    fecth_news_by_id = HackerNewsApi.Item.fetch("1")
+    assert fecth_news_by_id != nil
+  end
+
+  test "fecth_inexistend_news_by_id" do
+    fecth_news_by_id = HackerNewsApi.Item.fetch("0")
+    assert fecth_news_by_id == %{}
   end
 end
