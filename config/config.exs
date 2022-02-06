ae78b6aa6e0ff848fb29e7c0df7343b8f89fd610
noe perez chamorro <noeperezchamorro@noes-Mac-mini.local>
2022-02-06 10:28:34 -0600

    set_up phoenix app

--- config/config.exs
+++ config/config.exs
@@ -9,6 +9,21 @@
 # move said applications out of the umbrella.
 import Config
 
+config :hacker_news_web, HackerNewsWeb.Endpoint,
+  url: [host: "localhost"],
+  secret_key_base: "aNKNLzasPXS3lEylYDOmt54i0XnNU4Jh37AU67UzyENPqXg48TMPXNaIWfL3b7Ww",
+  render_errors: [view: HackerNewsWeb.ErrorView, accepts: ~w(html json), layout: false],
+  pubsub_server: HackerNewsWeb.PubSub,
+  live_view: [signing_salt: "WGoVT3AK"]
+
+# Configures Elixir's Logger
+config :logger, :console,
+  format: "$time $metadata[$level] $message\n",
+  metadata: [:request_id]
+
+# Use Jason for JSON parsing in Phoenix
+config :phoenix, :json_library, Jason
+
 # Sample configuration:
 #
 #     config :logger, :console,
--- config/test.exs
+++ config/test.exs
@@ -5,3 +5,14 @@ hacker_news_api_url = "https://hacker-news.firebaseio.com/v0"
 config :hacker_news_api, HackerNewsApi.Config,
   top_news: hacker_news_api_url <> "/topstories.json",
   news_info: hacker_news_api_url <> "/item/ID.json"
+
+use Mix.Config
+
+# We don't run a server during test. If one is required,
+# you can enable the server option below.
+config :hacker_news_web, HackerNewsWeb.Endpoint,
+  http: [port: 4002],
+  server: false
+
+# Print only warnings and errors during test
+config :logger, level: :warn
