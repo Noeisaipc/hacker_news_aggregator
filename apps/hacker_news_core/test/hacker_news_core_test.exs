e8510047f0e38f3952e2b8553fe68dba97f786d5
noe perez chamorro <noeperezchamorro@noes-Mac-mini.local>
2022-02-06 02:06:57 -0600

    add test for persistant top_50_news_ids in Agent

--- apps/hacker_news_core/test/hacker_news_core_test.exs
+++ apps/hacker_news_core/test/hacker_news_core_test.exs
@@ -3,6 +3,11 @@ defmodule HackerNewsCoreTest do
 
   # test_persistant_50_news_updated
 
+  test "test_persistant_50_news_updated" do
+    HackerNewsCore.fetch_top_news()
+    assert Enum.count(HackerNewsCore.Top50Agent.value) == 50
+  end
+
   # test_fetching_top_50_in_cache
 
   # test_fetching_news_by_id_in_cache
